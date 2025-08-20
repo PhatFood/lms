b29d3a589bf0a57b3bdcef9c71248a452e3e74a3
nguyenbaophat <phat.nb@kdc.vn>
2025-08-20 22:50:45 +0700

    update

--- frontend/src/pages/Courses.vue
+++ frontend/src/pages/Courses.vue
@@ -23,6 +23,7 @@
 			class="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:items-center justify-between mb-5"
 		>
 			<div class="text-lg text-ink-gray-9 font-semibold">
+				Why huhuhu
 				{{ __('All Courses') }}
 			</div>
 			<div
